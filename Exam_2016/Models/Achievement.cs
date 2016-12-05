6f14eeb4f72fcbe7a06bf8c80cc85fab77dcece5
Ralf Patrik Blaga <ralf.patric@yahoo.com>
2016-12-05 14:51:07 +0100

    Added yearly focus and achievements models

---
+++ Exam_2016/Models/Achievement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exam_2016.Models
+{
+    public class Achievement
+    {
+        public int AchievementId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Point { get; set; }
+
+        public bool Approved { get; set; }
+
+        public int RoleId { get; set; }
+    }
+}
---
+++ Exam_2016/Models/Yearlyfocus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exam_2016.Models
+{
+    public class Yearlyfocus
+    {
+        public int YearlyfocusId { get; set; }
+
+        public int Year { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public IEnumerable<Role> Roles { get; set; }
+    }
+}
