24c6cba9c8555db0acf736d011da0dc32254b5bb
Nadia <nadia@nadia.com>
2016-12-05 15:12:34 +0100

    employee model added

---
+++ Exam_2016/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exam_2016.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+        public int Score { get; set; }
+        public string ProfilePicture { get; set; }
+        public IEnumerable<Role> PastRoles { get; set; }
+        public IEnumerable<Role> CurrentRoles { get; set; }
+        public IEnumerable<Role> FutureRoles { get; set; }
+        public IEnumerable<Achievement> AchievementsEarned { get; set; }
+        public int NextYearlyinterview { get; set; }
+
+    }
+}
