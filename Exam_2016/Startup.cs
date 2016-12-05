bffb245b10c56f461c30be7077467142b9209831
Ralf Patrik Blaga <ralf.patric@yahoo.com>
2016-12-05 14:08:29 +0100

    Add project files.

---
+++ Exam_2016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Exam_2016.Startup))]
+namespace Exam_2016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
