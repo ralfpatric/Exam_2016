ae3e44bd017aeb30b6ba276dc9fb5099e869d547
Thao <Rot@SAM>
2016-12-07 21:12:50 +0100

    add link Create Company

---
+++ Exam_2016/Microsoft/AspNet/Identity/UserManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.AspNet.Identity
+{
+    internal class UserManager
+    {
+        internal static void AddToRole(object id, string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
