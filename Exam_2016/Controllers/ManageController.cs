242de2ab80cf5c3b5280ff02b8c5ff64ebac6451
Thao <Rot@SAM>
2016-12-08 11:23:34 +0100

    fdght

--- Exam_2016/Controllers/ManageController.cs
+++ Exam_2016/Controllers/ManageController.cs
@@ -77,6 +77,7 @@ namespace Exam_2016.Controllers
             };
             return View(model);
         }
+
         // GET: /Manage/EmployeeInfoEdit
         public ActionResult EmployeeInfoEdit()
         {
@@ -85,14 +86,32 @@ namespace Exam_2016.Controllers
             return View(employee);
         }
 
-        // GET: /Manage/EmployeeDetails 
+        // GET: /Manage/EmployeeDetails/id
         public ActionResult EmployeeDetails(string id)
         {
             //var CurrentUserId = User.Identity.GetUserId();
             Employee employee = db.Employees.Find(id);
             return View(employee);
         }
-        
+
+        // GET: Manage/EmployeeDelete/id
+        public ActionResult EmployeeDelete(string id)
+        {
+            Employee employee = db.Employees.Find(id);
+            return View(employee);
+        }
+
+        // POST: Manage/EmployeeDelete/id
+        [HttpPost, ActionName("EmployeeDelete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
 
         [HttpPost]
         public ActionResult SaveChanges(SaveChangesViewModel model)
@@ -110,9 +129,9 @@ namespace Exam_2016.Controllers
                 this.db.SaveChanges();
             }
 
-                return View("UserDetail");
+                return View("EmployeeInfoEdit");
         }
-        //
+        
         // POST: /Manage/RemoveLogin
         [HttpPost]
         [ValidateAntiForgeryToken]
